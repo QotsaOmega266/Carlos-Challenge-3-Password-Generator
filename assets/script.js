abbe02149be1156628e1f44d278e7b87e374d85b
QotsaOmega266 <cogle6095@gmail.com>
2023-11-19 15:25:27 -0700

    final commit with changes

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,20 @@ var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 
 //setup variables
-var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+var passwordCharacters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
 var passwordLength = 8;
-var password = "";
+var passwordVariable = "";
 
 
 // generates 8 randomized password characters and displays them in the box to the user 
 function createMyPassword() {
 
   for(var i = 0; i < passwordLength; i++){
-    var number = Math.floor(Math.random() * chars.length);
-    password += chars.substring(number, number + 1);
+    var number = Math.floor(Math.random() * passwordCharacters.length);
+    passwordVariable += passwordCharacters.substring(number, number + 1);
   };
 
-  passwordText.value = password;
+  passwordText.value = passwordVariable;
 
   
 };
